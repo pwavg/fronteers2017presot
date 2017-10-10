ff29846892798c18148a871cb1993abbcabdc6c7
Peter van Gennip <peter.van.gennip@valtech.nl>
2017-10-10 22:08:50 +0200

    Initial commit

---
+++ Gulpfile.js
@@ -0,0 +1,77 @@
+var autoprefixer    = require("gulp-autoprefixer")
+var browserify      = require("gulp-browserify")
+var concat          = require("gulp-concat")
+var connect         = require("gulp-connect")
+var data            = require("gulp-data")
+var gulp            = require("gulp")
+var nunjucksRender  = require("gulp-nunjucks-render")
+var plumber         = require("gulp-plumber")
+var rename          = require("gulp-rename")
+var sass            = require("gulp-sass")
+
+var paths = {
+    assets: {
+        fonts: "assets/fonts/",
+        img: "assets/img/",
+        js: "assets/js/",
+        css: "assets/css/"
+    },
+    fonts: "source/fonts/*.*",
+    img: "source/img/*.*",
+    html: "source/templates/slides.html",
+    js: "source/javascripts/**/*.js",
+    root: "./",
+    scss: "source/stylesheets/slides.scss",
+    allscss: "source/stylesheets/*.scss"
+}
+
+gulp.task("html", function() {
+    nunjucksRender.nunjucks.configure(["source/templates/"])
+    return gulp.src(paths.html)
+        .pipe(nunjucksRender())
+        .pipe(rename("index.html"))
+        .pipe(gulp.dest(paths.root))
+        .pipe(connect.reload())
+})
+
+gulp.task("js", function() {
+    return gulp.src(paths.js)
+        .pipe(plumber())
+        .pipe(browserify({
+            transform: ["babelify"],
+            debug: true,
+        }))
+        .pipe(gulp.dest(paths.assets.js))
+        .pipe(connect.reload())
+})
+
+gulp.task("sass", function() {
+    return gulp.src(paths.scss)
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(gulp.dest(paths.assets.css))
+        .pipe(connect.reload())
+})
+
+gulp.task("images", function () {
+    return gulp.src(paths.img)
+        .pipe(gulp.dest(paths.assets.img))
+        .pipe(connect.reload())
+})
+
+gulp.task("watch", function() {
+    gulp.watch(paths.js,   ["js"])
+    gulp.watch(paths.allscss, ["sass"])
+    gulp.watch(paths.html, ["html"])
+})
+
+gulp.task("connect", function() {
+    connect.server({
+        livereload: true,
+        port: 8000
+    })
+})
+
+gulp.task("default", ["build", "watch", "connect"])
+gulp.task("build", ["js", "sass", "html", "images"])
---
+++ source/javascripts/slides.js
@@ -0,0 +1,32 @@
+import 'babel-core/polyfill'
+import 'whatwg-fetch'
+
+import head   from 'reveal.js/lib/js/head.min'
+import Reveal from 'reveal.js/js/reveal'
+
+import loadDataSrcSVG from './svg'
+import loadPrintCSS   from './print'
+
+var prefix = 'node_modules/reveal.js/plugin'
+
+document.addEventListener('DOMContentLoaded', (event) => {
+	window.Reveal = Reveal
+	Reveal.initialize({
+		controls: false,
+		progress: true,
+		history: true,
+		center: true,
+		transition: 'slide',
+		dependencies: [
+			{ src: `${prefix}/markdown/marked.js`, condition: () => !!document.querySelector('[data-markdown]') },
+			{ src: `${prefix}/markdown/markdown.js`, condition: () => !!document.querySelector('[data-markdown]') },
+			{ src: `${prefix}/highlight/highlight.js`, async: true, condition: () => !!document.querySelector('pre code'), callback: () => hljs.initHighlightingOnLoad() },
+			{ src: `${prefix}/zoom-js/zoom.js`, async: true },
+			{ src: `${prefix}/notes/notes.js`, async: true },
+			{ src: `${prefix}/math/math.js`, async: true },
+		],
+	})
+
+	loadDataSrcSVG()
+	loadPrintCSS()
+})
